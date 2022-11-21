0d38a5c80b2ffd3101bf8ec1f240c785b7d77e48
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2022-11-21 19:07:34 +0530

    Time: 10 ms (79.05%), Space: 68.5 MB (62.70%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int nearestExit(char[][] grid, int[] entrance) {
+        int n=grid.length,m=grid[0].length;
+        LinkedList<Integer>que=new LinkedList<>();
+        int[][] dir={{1,0},{-1,0},{0,1},{0,-1}};
+        int startrow=entrance[0];
+        int startcol=entrance[1];
+        que.addLast(startrow*m + startcol);
+        grid[startrow][startcol]='+';
+        int level=0;
+        while(que.size()!=0){
+            int size=que.size();
+            level++;
+            while(size-->0){
+                int idx=que.removeFirst();
+                int sr=idx/m,sc=idx%m;
+                
+                for(int d=0;d<dir.length;d++){
+                    int r=sr+dir[d][0];
+                    int c=sc+dir[d][1];
+                    if(r>=0 && c>=0 && r<n && c<m&& grid[r][c]=='.'){
+                        if(r==0||r==n-1||c==0||c==m-1) return level;
+                        grid[r][c]='+';
+                        que.addLast(r*m+c);
+                    }
+                }
+            }
+        }
+        return -1;
+        
+    }
+}
