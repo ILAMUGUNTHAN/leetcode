75d568e6fb7d7b37f6d9df6e716d960e105ae750
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2022-11-23 18:44:05 +0530

    Time: 43 ms (5.77%), Space: 49.1 MB (6.81%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set set=new HashSet();
+        for(int i=0;i<9;++i){
+            for(int j=0;j<9;++j){
+                if(board[i][j]!='.'){
+                    String b="a"+board[i][j]+"a";
+                    if(!set.add(b+i)||!set.add(j+b)||!set.add(i/3 +b+ j/3)){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
