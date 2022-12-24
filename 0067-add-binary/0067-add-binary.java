fef7278cbc011613930f5627316517feb5efa07d
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2022-12-24 15:11:51 +0530

    Time: 5 ms (38.96%), Space: 42.6 MB (62.66%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,7 +1,6 @@
 import java.math.BigInteger;
 class Solution {
     public String addBinary(String a, String b) {
-        String res =(new BigInteger(a,2).add(new BigInteger(b,2))).toString(2);
-        return res;
+        return (new BigInteger(a,2).add(new BigInteger(b,2))).toString(2);
     }
 }
