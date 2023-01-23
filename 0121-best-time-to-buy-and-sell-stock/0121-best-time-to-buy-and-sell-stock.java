b27f4ba980b5070a02d2ca5b29ea0eb25eb5665b
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2023-01-23 21:27:45 +0530

    Time: 4 ms (27.93%), Space: 76.4 MB (33.91%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int ans=0;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            min=Math.min(min,arr[i]);
+            ans=Math.max(ans,arr[i]-min);
+        }
+        return ans;
+    }
+}
