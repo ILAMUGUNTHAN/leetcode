742925bd4ba68bf29bbe16371c69a5cbaeda691b
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2023-03-30 18:05:07 +0530

    Time: 1 ms (73.62%), Space: 40.2 MB (83.89%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,9 +1,9 @@
 class Solution {
     public String interpret(String command) {
         
-        if(command.contains("()")) command = command.replace("()","o");
-        if(command.contains("(al)")){
+        if(command.contains("(al)") || command.contains("()")){
             command = command.replace("(al)","al");
+            command = command.replace("()","o");
             interpret(command);
         }
         
