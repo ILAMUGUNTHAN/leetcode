8711f678e662a0adbec5892dfc7a8d373d148dd4
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2023-12-08 21:13:17 +0530

    Time: 100 ms (78.33%), Space: 61 MB (70.00%) - LeetHub

---
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public string Tree2str(TreeNode root) {
+        
+        if (root == null) return "";
+        
+        string result = root.val + "";
+        
+        string left = Tree2str(root.left);
+        String right = Tree2str(root.right);
+
+        if (left == "" && right == "") return result;
+        if (left == "") return result + "()" + "(" + right + ")";
+        if (right == "") return result + "(" + left + ")";
+        return result + "(" + left + ")" + "(" + right + ")";
+    }
+    
+    
+}
