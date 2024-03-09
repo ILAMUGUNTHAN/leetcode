6b0468755b9b33119b28109d6b33d8af0a613f21
ILAMUGUNTHAN N <86196624+ILAMUGUNTHAN@users.noreply.github.com>
2024-03-09 20:17:33 +0530

    Time: 223 ms (90.12%), Space: 66.3 MB (27.16%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int GetCommon(int[] nums1, int[] nums2) {
+        int p1 = 0;
+        int p2 = 0;
+        
+        while(p1<nums1.Length && p2<nums2.Length)
+        {
+            if(nums1[p1]==nums2[p2])
+            {
+                return nums1[p1];
+            }
+            else if(nums1[p1]<nums2[p2])
+            {
+                p1++;
+            }
+            else
+            {
+                p2++;
+            }
+                        
+        }
+        
+        
+        return -1;
+    }
+}
